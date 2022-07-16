e24c15720a77301aa057b9b25fe60a49888c6f0b
ArockiaSybilPerez <109013417+ArockiaSybilPerez@users.noreply.github.com>
2022-07-16 00:28:52 -0700

    Update ReverseEvenWords.java

--- ReverseEvenWords.java
+++ ReverseEvenWords.java
@@ -5,23 +5,25 @@ public class ReverseEvenWords {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Declare the input
-		String G1 = "I AM A SOFTWARE TESTER";
-		//split the words
-		String G2[] = G1.split(" ");
+		String sentence = "I AM A SOFTWARE TESTER";
 		//Traverse through each word (using loop)
-		for(int i=0; i<G2.length; i++) {
+		for(String s:sentence.split(" ")) {
 			//find the odd index within the loop (use mod operator)
-			if(i%2==0) {
+			if(s.length()%2==0) {
 				//split the words and have it in an array
-				char[] array = G2[i].toCharArray();
+				char[] array = s.toCharArray();
 				//print the even position words in reverse order using another loop
 				for(int j=array.length-1;j>=0;j--) {
 					System.out.print(array[j]);
+					
 				}
+				System.out.print("  ");
+				System.out.println('\n');
 			}
 			else {
 				//Print Even String with Space
-				System.out.print(" "+G2[i]+" ");
+				System.out.print(s.toString());
+				System.out.println('\n');
 			}
 		}
 }
